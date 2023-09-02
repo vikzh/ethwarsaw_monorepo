5143182797fa7f7e283d8371ac61cbd80f59d051
vikzh <vikt.anmail@gmail.com>
2023-09-02 20:58:22 +0200

    username password abstract added

--- extension/src/Login.js
+++ extension/src/Login.js
@@ -3,7 +3,21 @@ import {Button} from "antd";
 import {ethers} from "ethers";
 import {useNavigate} from "react-router-dom";
 import {useState} from "react";
-
+import {
+    AlipayCircleOutlined,
+    LockOutlined,
+    MobileOutlined,
+    TaobaoCircleOutlined,
+    UserOutlined,
+    WeiboCircleOutlined,
+} from '@ant-design/icons';
+import {
+    LoginForm,
+    ProConfigProvider,
+    ProFormCaptcha,
+    ProFormCheckbox,
+    ProFormText,
+} from '@ant-design/pro-components';
 function Login({setWallet, setSeedPhrase}) {
     const [newSeedPhrase, setNewSeedPhrase] = useState(null);
     const navigate = useNavigate();
@@ -18,20 +32,49 @@ function Login({setWallet, setSeedPhrase}) {
 
     return (
         <>
-            <div className="content">
-                Login page
-            </div>
 
-            <input/>
+            <>
+                <ProFormText
+                    name="username"
+                    fieldProps={{
+                        size: 'large',
+                        prefix: <UserOutlined className={'prefixIcon'} />,
+                    }}
+                    placeholder={'username'}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'user name is required!',
+                        },
+                    ]}
+                />
+                <ProFormText.Password
+                    name="password"
+                    fieldProps={{
+                        size: 'large',
+                        prefix: <LockOutlined className={'prefixIcon'} />,
+                    }}
+                    placeholder={'password'}
+                    rules={[
+                        {
+                            required: true,
+                            message: 'password is required！',
+                        },
+                    ]}
+                />
+            </>
             <Button
+                style={{width: "100%"}}
+                className="ant-btn ant-btn-primary ant-btn-lg"
                 onClick={() => generateWallet()}
                 type="primary">
                 Login
             </Button>
             <Button
-                type="primary"
+                style={{marginTop: "25px"}}
+                type="text"
                 onClick={() => navigate("/")}>
-                Home
+                ⬅ Home
             </Button>
         </>
     )
